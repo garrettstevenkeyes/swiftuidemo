7f94ebe971cb1ccab8d7e10cea591b5fdaa524dc
Garrett Keyes <garrett.keyes@gmail.com>
2024-02-11 19:32:14 -0500

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Garrett Keyes on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
